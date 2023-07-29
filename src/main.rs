c3dd006e1c97a3ad5756b379c65dbadd0ab790d2
Oliver Säfström <safstrom.oliver@gmail.com>
2023-07-29 17:42:09 +0200

    moving cars

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use rand::Rng;
 
 const PLAYER_SPEED: f32 = 500.0;
 const PLAYER_PADDING: f32 = 10.0;
-const PLAYER_SIZE: Vec2 = Vec2::new(6.0, 20.0);
+const PLAYER_SIZE: Vec2 = Vec2::new(5.0, 8.0);
 const CAR_SIZE: Vec2 = Vec2::new(20.0, 50.0);
+const INITIAL_CAR_DIRECTION: Vec2 = Vec2::new(0.0, -0.5);
+const CAR_SPEED: f32 = 400.0;
 const WALL_THICKNESS: f32 = 10.0;
 
 // x coordinates
@@ -16,9 +19,10 @@ const TOP_WALL: f32 = 300.;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .insert_resource(SpawnTimer(Timer::from_seconds(0.1, TimerMode::Repeating)))
+        .add_plugins((DefaultPlugins, ))
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (move_player,spawn_car))
+        .add_systems(FixedUpdate, (move_player, spawn_car, apply_velocity))
 
         .run();
 }
@@ -29,6 +33,11 @@ struct Player;
 #[derive(Component)]
 struct Car;
 
+#[derive(Resource)]
+struct SpawnTimer(Timer);
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
 
 fn setup(
     mut commands: Commands,
@@ -42,26 +51,39 @@ fn setup(
             custom_size: Some(PLAYER_SIZE),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+        transform: Transform::from_translation(Vec3::new(-50., BOTTOM_WALL + 20.0, 0.)),
         ..default()
     }, Player));
 }
 
 fn spawn_car(
-    mut commands: Commands
+    mut commands: Commands,
+    time: Res<Time>,
+    mut timer: ResMut<SpawnTimer>,
 ) {
-    commands.spawn(
-        (SpriteBundle {
-            sprite: Sprite {
-                color: Color::BLACK,
-                custom_size: Some(CAR_SIZE),
+    let mut rng = rand::thread_rng();
+    if timer.0.tick(time.delta()).just_finished() {
+        commands.spawn(
+            (SpriteBundle {
+                sprite: Sprite {
+                    color: Color::BLACK,
+                    custom_size: Some(CAR_SIZE),
+                    ..default()
+                },
+                transform: Transform::from_translation(Vec3::new(rng.gen_range(LEFT_WALL..RIGHT_WALL), TOP_WALL, 1.)),
                 ..default()
-            },
-            transform: Transform::from_translation(Vec3::new(-50., 50., 1.)),
-            ..default()
-        }, Car));
+            }, Car,
+             Velocity(INITIAL_CAR_DIRECTION.normalize() * CAR_SPEED)
+            ));
+    }
 }
 
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<FixedTime>) {
+    for (mut transform, velocity) in &mut query {
+        transform.translation.x += velocity.x * time_step.period.as_secs_f32();
+        transform.translation.y += velocity.y * time_step.period.as_secs_f32();
+    }
+}
 
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
